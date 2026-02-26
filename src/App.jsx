770f0df907b39be15350205befad68fa147efdfa
Abhijit Wankhade <abhijeetwankhade816@gmail.com>
2026-02-26 11:53:06 +0530

    configuration of tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div className='text-wh'>
-      hello world
-      </div>
-    </>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-4xl font-bold text-blue-600'>
+        Hello Tailwind CSS!
+      </h1>
+    </div>
   )
 }
 
